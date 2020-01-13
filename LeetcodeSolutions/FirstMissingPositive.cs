5811d64b9ffc868cd46e7c65f335e6c72c2f5a6d
scomert <sinancomert1@gmail.com>
2020-01-13 18:08:03 +0300

    Add project files.

---
+++ LeetcodeSolutions/FirstMissingPositive.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace LeetcodeSolutions
+{
+    public class FirstMissingPositive
+    {
+        public int Solution(int[] nums)
+        {
+            var d = nums
+                .Distinct()
+                .ToDictionary(key => key, val => val);
+
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                if (!d.ContainsKey(i))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
