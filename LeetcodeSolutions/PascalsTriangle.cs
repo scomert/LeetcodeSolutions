d62001426e5887ae5b3b57f8ee54526af1c6b865
scomert <sinancomert1@gmail.com>
2020-01-13 20:55:37 +0300

    added pascalstriangle

---
+++ LeetcodeSolutions/PascalsTriangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions
+{
+    public class PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            if (numRows == 0) return new int[0][];
+
+            var levels = new int[numRows][];
+
+            levels[0] = new int[] { 1 };
+
+            for (int i = 1; i < numRows; i++)
+            {
+                var prevLevel = levels[i - 1];
+                var level = new int[i + 1];
+
+                level[0] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    level[j] = prevLevel[j - 1] + prevLevel[j];
+                }
+
+                level[i] = 1;
+
+                levels[i] = level;
+            }
+
+            return levels;
+        }
+    }
+}
