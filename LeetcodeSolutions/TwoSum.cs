8c0d615716f88aec35b654d72ef71e435d9a0341
scomert <sinancomert1@gmail.com>
2020-01-14 22:58:12 +0300

    added twosum

---
+++ LeetcodeSolutions/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace LeetcodeSolutions
+{
+    public class TwoSum
+    {
+        public int[] Solve(int[] nums, int target)
+        {
+            var complements = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (complements.TryGetValue(nums[i], out int index))
+                {
+                    return new int[] { index, i };
+                }
+                var complement = target - nums[i];
+                complements.TryAdd(complement, i);
+            }
+
+            return new int[0];
+        }
+    }
+}
